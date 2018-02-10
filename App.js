a55d906dd5e717008626c9c7379251b2395d7c23
CajunAzn007 <kevinw326@gmail.com>
2018-02-10 10:16:02 -0500

    added page stuff

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import {
   Text,
   View,
   StyleSheet
 } from 'react-native';
-
+import{ Tabs, Root } from "./routers.js"
 import MainList from './src/mainPage'
 import Scanner from './src/Scanner'
+
 export default class App extends Component {
     render () {
         return(
-        <MainList>
-
-        </MainList>
+        <Root />
     )};
 }
--- src/mainPage.js
+++ src/mainPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, FlatList, StyleSheet, Text, View, 
+import { AppRegistry, FlatList, StyleSheet, Text, View,
   AsyncStorage,
   Button,
   TextInput,
@@ -9,21 +9,21 @@ import { AppRegistry, FlatList, StyleSheet, Text, View,
 export default class MainList extends Component {
 	state = {
 		names: [], //items of form (name, price, quantity)
-        prices: [],
-        quantities: [],
-        pictures: [],
+    prices: [],
+    quantities: [],
+    //pictures: [],
 		totalprices: 0
 	};
-    
+
     //adds item to cart at the end of cart
-	addToCart(name, price, quantity, picture){
+	addToCart(name, price, quantity){
 		this.setState(
 			(prevState) => {
 				return {
 					names: prevState.names.push(name),
 					prices: prevState.prices.push(price),
 					quantities: prevState.quantities.push(quantity),
-					pictures: prevState.pictures.push(picture),
+					//pictures: prevState.pictures.push(picture),
 					totalprices: prevState.totalprices + price*quantity,
 				};
 			},
@@ -33,47 +33,70 @@ export default class MainList extends Component {
 
     //removes item from cart at index index
     deleteFromCart(index){
-        let requires = index < this.state.names.length
+      let requires = index < this.state.names.length
+  		if(requires) {
+  			this.setState(
+  				(prevState) => {
+  					let quantity = prevState.quantities[index];
+  					let price = prevState.prices[index];
+  					if(quantity = 1){
+  						return{
+  							names: prevState.names.slice(index,1),
+  							prices: prevState.prices.slice(index,1),
+  							quantities: prevState.quantities.slice(index,1),
+  							//pictures: prevState.pictures.slice(index,1),
+  							totalprices: prevState.totalprices - price,
 
-		if(requires) {
-			this.setState(
-				(prevState) => {
-					let quantity = prevState.quantities[index];
-					let price = prevState.prices[index];
-					if(quantity = 1){
-						return{
-							names: prevState.names.slice(index,1),
-							prices: prevState.prices.slice(index,1),
-							quantities: prevState.quantities.slice(index,1),
-							pictures: prevState.pictures.slice(index,1),
-							totalprices: prevState.totalprices - price,
-
-						};
-					};
-					prevState.quantities[index] = quantity - 1;
-					return {
-						names: prevState.names,
-						prices: prevState.prices,
-						quantities: prevState.quantities,
-						pictures: prevState.pictures.slice(index,1),
-						totalprices: prevState.totalprices - price,
-					};
-				},
-				() => AsyncStorage.setItem("CART", this.state)//need function in Cart that saves state
-			);
-		}
+  						};
+  					};
+  					prevState.quantities[index] = quantity - 1;
+  					return {
+  						names: prevState.names,
+  						prices: prevState.prices,
+  						quantities: prevState.quantities,
+  						//pictures: prevState.pictures.slice(index,1),
+  						totalprices: prevState.totalprices - price,
+  					};
+  				},
+  				() => AsyncStorage.setItem("CART", this.state)//need function in Cart that saves state
+  			);
+  		}
     };
 
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
       	<Text> HEllo World! </Text>
       </View>
     );
   }
-    
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  list: {
+    width: "100%"
+  },
+  listItem: {
+    paddingTop: 2,
+    paddingBottom: 2,
+    fontSize: 18
+  },
+  hr: {
+    height: 1,
+    backgroundColor: "gray"
+  },
+  listItemCont: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+});
 
 module.export = MainList;
