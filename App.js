09456e2d07d3d4967a04eb16db2a0081f28af358
yuey1203 <yang.yue.120398@gmail.com>
2018-02-10 04:19:47 -0500

    adding App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+
+<<<<<<< HEAD
+import MainList from './src/mainPage'
+import Scanner from './src/Scanner'
+export default class App extends Component {
+    render () {
+        return(
+        <MainList>
+
+        </MainList>
+    )};
+}
